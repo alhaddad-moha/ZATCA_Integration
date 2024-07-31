458c15479b760fa145728e100268c65a9122d4d6
alhaddad-moha <mohammed@wosul.sa>
2024-07-31 23:41:31 +0300

    add fluent validation

---
+++ ZATCA-V2/CustomValidators/BulkInvoiceRequestValidator.cs
@@ -0,0 +1,27 @@
+﻿using ZATCA_V2.Repositories.Interfaces;
+
+namespace ZATCA_V2.CustomValidators;
+
+using FluentValidation;
+using ZATCA_V2.Requests;
+
+public class BulkInvoiceRequestValidator : AbstractValidator<BulkInvoiceRequest>
+{
+    private readonly ICompanyRepository _companyRepository; 
+    public BulkInvoiceRequestValidator(ICompanyRepository companyRepository)
+    {
+        _companyRepository = companyRepository;
+        
+        
+        RuleForEach(x => x.Invoices).SetValidator(new InvoiceValidator());
+
+        RuleFor(x => x.InvoicesType).SetValidator(new InvoiceTypeValidator());
+        RuleFor(p => p.companyId)
+            .GreaterThan(0)
+            .MustAsync(async (id, token) =>
+            {
+                var companyExists = await _companyRepository.GetById(id);
+                return companyExists != null;
+            }).WithMessage("{PropertyName} does not exist");
+    }
+}
---
+++ ZATCA-V2/CustomValidators/CustomerInformationValidator.cs
@@ -0,0 +1,15 @@
+﻿namespace ZATCA_V2.CustomValidators;
+
+using FluentValidation;
+using ZATCA_V2.Requests;
+
+public class CustomerInformationValidator : AbstractValidator<CustomerInformation>
+{
+    public CustomerInformationValidator()
+    {
+        RuleFor(x => x.CommercialNumber).NotEmpty().WithMessage("Commercial Number is required.");
+        RuleFor(x => x.CommercialNumberType).NotEmpty().WithMessage("Commercial NumberType is required.");
+        RuleFor(x => x.RegistrationName).NotEmpty().WithMessage("Registration Name is required.");
+        RuleFor(x => x.RegistrationNumber).NotEmpty().WithMessage("Registration Number is required.");
+    }
+}
